2f4af7cba799a23c01b539f2aff917796b10eeba
mtkozuki <matheusbkozuki@gmail.com>
2020-04-06 22:34:25 -0300

    4/6 daily coding review

---
+++ pythonPrinciples/BasicOperatosNInput.py
@@ -0,0 +1,30 @@
+# Input
+print("What is your name?")
+#name = input()
+#print('Hello, ', name)
+
+# Operators
+# Basic + - / *
+num1 = 45
+num2 = 94
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 * num2)
+
+# Exponential **
+print(num1 ** num2)
+
+# Integer division //
+print (num1 // num2)
+
+# Remainer %
+print(num1 % num2)
+
+# Sum program
+print('Pick a number:')
+num1 = input()
+print('Pick another number')
+num2 = input()
+SUM = int(num1) + int(num2) # if you don't convert here you will end up with string concatenation
+print(SUM)
---
+++ pythonPrinciples/VariablesNDatatypes.py
@@ -0,0 +1,24 @@
+#Four main data types in python
+
+#Int or Integer
+int 0, 1, 2, 4
+
+#Str or String
+str 'Koz', "Hello", "ye'ah", 'ye"ah'
+
+#Bool
+bool True, False
+
+#Float (Decimals)
+float 0.32, 1.23
+
+#Creating variables
+# Syntax:
+# name = value
+name = 'Koz'
+age = 24
+hasGirl = False
+money = 0.00
+# Rules:
+#  Can only contain text, numbers and underscore AND cannot start with numbers
+#  Case sensitive. A variable called name will be different than Name or NAME
