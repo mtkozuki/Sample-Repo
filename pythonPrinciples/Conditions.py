4d222e96d2c40652e07102b6d86d53f615bfd6da
mtkozuki <matheusbkozuki@gmail.com>
2020-04-06 22:53:59 -0300

    4/6 more two just for fun

---
+++ pythonPrinciples/Conditions.py
@@ -0,0 +1,7 @@
+# comparison operators return a value of True or False
+# < > == !=
+4 < 5 # True. 4 is less than 5
+6 > 7 # False 6 isnt greater than 7
+'me' == 'you' # False these strings are not equal
+6 != 7 # True 6 is not equal 7
+
---
+++ pythonPrinciples/ifelseelif.py
@@ -0,0 +1,14 @@
+# Flow
+# Syntax:
+# if condition:
+#   do this
+number1 = int(input('Send me a number: '))
+number2 = int(input('Send me another number: '))
+if number1 > number2:
+    print(number1,'is greater than', number2)
+elif number1 < number2:
+    print(number1, 'is less than', number2)
+else:
+    print(number1, 'is equal', number2)
+
+# More conditional operators: <= lte >= gte
